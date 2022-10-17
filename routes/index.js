b68236dc100c74a70eaa474277ae89b1b6a77f51
dkqp <ltg0513@gmail.com>
2022-10-17 14:32:51 +0900

    Set prettierrc.json 내용 추가
    prettier setting add

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function renderHome(req, res) {
-  res.render("index", { title: "Express" });
+router.get('/', function renderHome(req, res) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
