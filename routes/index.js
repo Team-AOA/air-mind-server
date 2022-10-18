efb049f0dca9849d91cd1dae02078057dc4d0497
dkqp <ltg0513@gmail.com>
2022-10-18 14:44:47 +0900

    Set extra setting
    의존성 모듈, mongodb 설정, router 관련 폴더 구조 등 수정

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function renderHome(req, res) {
-  res.render('index', { title: 'Express' });
-});
+const { auth } = require('./middlewares/authMiddleware');
+
+router.get('/users/:userId/mind-maps/:mindMapId/nodes/:nodeId', auth);
 
 module.exports = router;
---
+++ routes/middlewares/authMiddleware.js
@@ -0,0 +1,9 @@
+export function auth(req, res, next) {
+  next();
+}
+
+export function auth2(req, res, next) {
+  next();
+}
+
+module.exports.auth = auth;
