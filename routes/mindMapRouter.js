aa1676870e08f3a8da985866516c566fb65251fe
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-10-22 22:57:59 +0900

    Feat login 로직 작성

--- routes/mindMapRouter.js
+++ routes/mindMapRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { auth } = require('./middlewares/authMiddleware');
+const auth = require('./middlewares/authMiddleware');
 const {
   postHeadNodeData,
   getMindMapData,
