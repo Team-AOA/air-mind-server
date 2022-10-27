4642fd879612be5312302c4e9f24fe348d6da52e
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-10-27 18:47:57 +0900

    Fix key 환경변수 추가

--- configs/firebaseConfig.js
+++ configs/firebaseConfig.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
+// const serviceAccount = require('./serviceAccountKey.json');
+// eslint-disable-next-line no-useless-escape
+const serviceAccount = process.env.FIREBASE_SERVICE_KEY.replace(/\@/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
