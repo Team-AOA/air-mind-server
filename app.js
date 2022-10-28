9babecb7d7fd274bf129572a08357265aaa45910
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-10-28 11:41:23 +0900

    Chore 클라이언트 url 환경변수 수정

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/tmpImages', express.static('./tmpImages'));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
